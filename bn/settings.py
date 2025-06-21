a4108206681d8e8aef2ee9b132aba00ba9525fa0
Mumit Prottoy <mumitprottoy@gmail.com>
2025-06-21 19:49:19 +0600

    fixed typo causing 502

--- bn/settings.py
+++ bn/settings.py
@@ -34,18 +34,18 @@ INSTALLED_APPS = [
     'profiles',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
-    'corsheaders.Middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES' : (
