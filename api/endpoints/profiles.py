9764af5e6cf32bc4d7ac643757cf4ecebbffc3c7
Mumit Prottoy <mumitprottoy@gmail.com>
2025-06-22 12:47:25 +0600

    attached stats with profile info

--- api/endpoints/profiles.py
+++ api/endpoints/profiles.py
@@ -1,5 +1,7 @@
 from .libs import *
 from profiles.models import Follow
+from utils import subroutines as sr
+
 
 class PlayerProfileAPI(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -11,6 +13,7 @@ class PlayerProfileAPI(views.APIView):
         followers = [f.follower.details for f in Follow.objects.filter(followed=request.user)]
         profile['follower_count'] = followers.__len__()
         profile['followers'] = followers
+        profile['stats'] = sr.get_clean_dict(request.user.stats)
         return Response(profile, status=status.HTTP_200_OK)
     # def post(self, request: Request) -> Response:
     #     User.objects.filter(username=request.user.username)
