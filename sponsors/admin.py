a0d6a9224118932c10a7effc68a64dcd4ca35c28
Mumit Prottoy <mumitprottoy@gmail.com>
2025-07-13 21:32:46 +0600

    migrated changes v3

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -5,7 +5,5 @@ from django.contrib.admin.sites import AlreadyRegistered
 app_models = apps.get_app_config(__name__.split('.')[-2]).get_models()
 
 for model in app_models:
-    try:
-        admin.site.register(model)
-    except AlreadyRegistered:
-        pass
+    try:admin.site.register(model)
+    except AlreadyRegistered:pass
