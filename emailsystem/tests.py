7bad11a579f4ad7d1940552ef4444dce5bb4a046
Mumit Prottoy <mumitprottoy@gmail.com>
2025-07-29 15:19:54 +0600

    changed endpoint for csv upload API

--- emailsystem/tests.py
+++ emailsystem/tests.py
@@ -4,12 +4,12 @@ from .engine import EmailEngine
 
 def test_email() -> None:
     verification_code = random.randint(176524, 984561)
-    context = dict(code=verification_code)
+    context = dict(code=verification_code, full_name='Mumit Prottoy')
     subject = f'Your Email Verification Code [{verification_code}]'
     engine = EmailEngine(
         recipient_list=['mumitprottoy@gmail.com'],
         subject=subject,
-        template='emails/base.html',
+        template='emails/verification_code.html',
         context=context
     )
     engine.send()
