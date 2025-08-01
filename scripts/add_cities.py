fa82eeafbcdca355d4e6ed1b651f514b234399be
Mumit Prottoy <mumitprottoy@gmail.com>
2025-08-01 18:30:42 +0600

    fixed

--- scripts/add_cities.py
+++ scripts/add_cities.py
@@ -2,11 +2,12 @@ from tqdm import tqdm
 from data.countries import country_list
 from profiles.models import CityAndCountry
 
+
 def add_city() -> None:
     for country_data in tqdm(country_list, desc='Adding countries', unit=' country', leave=True):
         for city in tqdm(country_data['cities'], desc=f'Adding cities of {country_data['name']}', unit=' city'):
             CityAndCountry.objects.get_or_create(
-                city=city,
+                city=city['name'],
                 country=country_data['name']
             )
 
