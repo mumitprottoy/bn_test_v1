585e766b28af8ef9b9cd0f464854b5efdbe23310
Mumit Prottoy <mumitprottoy@gmail.com>
2025-11-17 03:06:35 +0600

    okay

--- beta/models.py
+++ beta/models.py
@@ -1,6 +1,9 @@
+import time
 from django.db import models
 from player.models import User
 from utils.keygen import KeyGen
+from emailsystem.engine import EmailEngine
+
 
 def get_key() -> str: return KeyGen().timestamped_alphanumeric_id(20)
 
@@ -31,5 +34,38 @@ class BetaTester(models.Model):
             user.save()
         
         return tester
+    
+    @classmethod
+    def send_emails_to_all(
+        cls: 'BetaTester', subject: str, template: str, context: dict=dict(), test: bool=False) -> None:
+        _context = context.copy()
+        connection = EmailEngine([], subject, template).get_connection_only()
+        connection.open()
+        batch_size = 50
+        testers = list(cls.objects.all())
+        
+        if test:
+            user = User.objects.get(email='mumitprottoy@gmail.com')
+            tester, _ = cls.objects.get_or_create(user=user)
+            testers = [tester]
 
+        for i, tester in enumerate(testers):
+            _context['full_name'] = tester.user.get_full_name()
+            engine = EmailEngine(
+                recipient_list=[tester.user.email],
+                subject=subject,
+                template=template,
+                context=_context,
+            )
+            email_obj = engine.setup_email(connection)
+            email_obj.send()
 
+            if (i + 1) % batch_size == 0:
+                connection.close()
+                time.sleep(1)
+                connection.open()
+
+            print(f'{i + 1} / {testers.__len__()} Sent to: {tester.user.email}')
+
+    def __str__(self) -> str:
+        return self.user.get_full_name()
