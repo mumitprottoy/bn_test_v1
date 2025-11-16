353c927bafa21c47abc7fa3af8b708a2807bbf65
Mumit Prottoy <mumitprottoy@gmail.com>
2025-11-16 20:03:39 +0600

    added beta tester

--- beta/models.py
+++ beta/models.py
@@ -1,9 +1,35 @@
 from django.db import models
 from player.models import User
+from utils.keygen import KeyGen
 
+def get_key() -> str: return KeyGen().timestamped_alphanumeric_id(20)
 
-class Tester(models.Model):
+class BetaTester(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    has_added_creds = models.BooleanField(default=False)
-    private_key=models
+    private_key=models.CharField(max_length=100, default=get_key)
+
+    @classmethod
+    def create(
+        cls: 'BetaTester', first_name: str, last_name: str, email: str) -> 'BetaTester':
+        created = False
+        user = User.objects.filter(email=email)
+        if user.exists():
+            user.update(first_name=first_name, last_name=last_name)
+            user=user.first()
+        else:
+            user = User.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                username=KeyGen().alphanumeric_key(20),
+                email=email
+            )
+            created = True
+            
+        tester = cls.objects.create(user=user)
+        if created:
+            user.username = f'beta_tester_{tester.id}'
+            user.save()
+        
+        return tester
+
 
