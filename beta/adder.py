6bbed00bfe639b8a6da0fb9129a2bff08f1ff51f
Mumit Prottoy <mumitprottoy@gmail.com>
2025-11-17 02:33:18 +0600

    okay

---
+++ beta/adder.py
@@ -0,0 +1,13 @@
+import csv
+from .models import BetaTester
+
+
+def add_testers(file_path: str) -> None:
+    file = open(file_path)
+    reader = csv.DictReader(file)
+    for row in reader:
+        BetaTester.create(**row)
+
+
+def add_batch_1():
+    add_testers('beta_testers_1.csv')
