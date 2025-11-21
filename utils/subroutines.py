e8961c2aefbd041c5aadc6c14e3db7a2f8fd9b37
Mumit Prottoy <mumitprottoy@gmail.com>
2025-11-21 21:39:17 +0600

    added new feature: Lanes

--- utils/subroutines.py
+++ utils/subroutines.py
@@ -1,7 +1,7 @@
 import datetime
 from django.utils import timezone as tz
 from datetime import datetime
-from django.utils.timezone import now, make_aware, get_current_timezone
+from django.utils.timezone import now, make_aware, get_current_timezone, is_naive
 from django.utils.timesince import timesince
 
 
@@ -18,7 +18,8 @@ def get_clean_dict(obj, target_key: str='_state') -> dict:
 
 
 def pretty_timesince(dt):
-    dt = make_aware(dt, timezone=get_current_timezone())
+    if is_naive(dt):
+        dt = make_aware(dt, timezone=get_current_timezone())
     if not isinstance(dt, datetime):
         return ""
 
